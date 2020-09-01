7bf89f9da5bc62d9ed54ec65cb83f84fbfc86eba
Sarah Kerr <sarah.kerr93@gmail.com>
2020-08-31 21:21:23 -0400

    added seed file

---
+++ db/seed.sql
@@ -0,0 +1,6 @@
+INSERT INTO Employees
+    (first_name, last_name, birthday, email, hire_date, orientation, compliance_training)
+VALUES
+    (Sarah, Kerr, 1993-09-24, sarah@gmail.com, 2020-08-28, 2020-08-29, 2020-08-29),
+    (Jane, Doe, 1990-05-18, jane@gmail.com, 2019-08-28, 2019-09-02, 2019-10-30),
+    (Tyler, Smith, 1989-08-29, tyler@gmail.com, 2017-10-11, 2017-11-01, 2018-01-02); 
