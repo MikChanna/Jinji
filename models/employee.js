f4f01f5978082632ac9215dac1b173814e597b71
Sarah Kerr <sarah.kerr93@gmail.com>
2020-09-05 11:56:40 -0400

    new employee add now works, need to incorporate allergies and hobbies later

--- models/employee.js
+++ models/employee.js
@@ -1,74 +1,59 @@
 const models = require("../models");
 
 module.exports = function(sequelize, DataTypes) {
-  const Employee = sequelize.define(
-    "Employee",
-    {
-      id: {
-        type: DataTypes.INTEGER,
-        autoIncrement: true,
-        primaryKey: true,
+  const Employee = sequelize.define("Employee", {
+    id: {
+      type: DataTypes.INTEGER,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    first_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2],
       },
-      first_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [2],
-        },
+    },
+    last_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [2],
       },
-      last_name: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [2],
-        },
+    },
+    birthday: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
+    email: {
+      type: DataTypes.STRING,
+      allowNull: false,
+      validate: {
+        len: [5],
       },
-      birthday: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
-      email: {
-        type: DataTypes.STRING,
-        allowNull: false,
-        validate: {
-          len: [5],
-        },
-      },
-      // allergyID: {
-      //   type: DataTypes.INTEGER,
-      //   allowNull: true,
-      //   foreignKey: true,
-      // },
-      // hobbyID: {
-      //   type: DataTypes.INTEGER,
-      //   allowNull: true,
-      //   foreignKey: true,
-      // },
-      hire_date: {
-        type: DataTypes.DATEONLY,
-        allowNull: false,
-      },
+    },
+    // allergyID: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: true,
+    //   foreignKey: true,
+    // },
+    // hobbyID: {
+    //   type: DataTypes.INTEGER,
+    //   allowNull: true,
+    //   foreignKey: true,
+    // },
+    hire_date: {
+      type: DataTypes.DATEONLY,
+      allowNull: false,
+    },
 
-      orientationComplete: {
-        type: DataTypes.DATEONLY,
-      },
-      compliance_trainingComplete: {
-        type: DataTypes.DATEONLY,
-      },
-    }
-    // {
-    //   include: [
-    //     {
-    //       association: models.Hobbies,
-    //       as: "hobbies",
-    //     },
-    //     {
-    //       association: models.Allergies,
-    //       as: "allergies",
-    //     },
-    //   ],
-    // }
-  );
+    orientationComplete: {
+      type: DataTypes.DATEONLY,
+    },
+    compliance_trainingComplete: {
+      type: DataTypes.DATEONLY,
+    },
+  });
 
   // Employee.associate = function(models) {
   //   Employee.belongsToMany(models.Allergies, {
